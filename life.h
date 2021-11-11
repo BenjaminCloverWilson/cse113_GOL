1dd3bb42a61e58afbcc7355ce0a206f021a9fea8
BenjaminCloverWilson <89162179+BenjaminCloverWilson@users.noreply.github.com>
2021-11-11 00:27:58 -0700

    Update life.h
    Initial pattern finished

--- life.h
+++ life.h
@@ -17,7 +17,7 @@
 #include <stdlib.h>
 #include <errno.h>
 
-#define LEN 32
+#define LEN 256
 
 /* Matrix functions */
 unsigned char **init_matrix(int row, int col);
@@ -25,7 +25,7 @@ void print_matrix(unsigned char **matrix, int row, int col);
 unsigned char **next_gen_hedge(unsigned char **matrix, int row, int col);
 unsigned char **next_gen_torus(unsigned char **matrix, int row, int col);
 unsigned char **next_gen_klein(unsigned char **matrix, int row, int col);
-unsigned char **pattern(FILE *fp, int row, int col);
+unsigned char **pattern(FILE *fp, int row, int col, int x_o, int y_o);
 void free_matrix(unsigned char **matrix, int row);
 
 #endif
