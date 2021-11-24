a7483a0c5f3eb30f5e4fcd0e4da509085647ad09
BenjaminCloverWilson <89162179+BenjaminCloverWilson@users.noreply.github.com>
2021-11-23 18:40:55 -0700

    Update life.h
    Fixed comments

--- life.h
+++ life.h
@@ -1,13 +1,8 @@
 /**
  * @file life.h
  * @brief Header file for life.c
- * @details Contains function prototypes, definitions, and #include
- * statements for life.c, and basically stuff for Conway's Game of Life
- * outside of SDL graphics.
  * @author Benjamin Wilson
  * @date Fall 2021
- * @todo I don't know what I'm even doing yet
- * @bug none
  */
 
 #ifndef LIFE_H_
@@ -21,13 +16,13 @@
 
 #define LEN 256
 
-/* Matrix functions */
 unsigned char **init_matrix(int row, int col);
 void print_matrix(unsigned char **matrix, int row, int col);
 unsigned char **next_gen_hedge(unsigned char **matrix, int row, int col);
 unsigned char **next_gen_torus(unsigned char **matrix, int row, int col);
 unsigned char **next_gen_klein(unsigned char **matrix, int row, int col);
-unsigned char **pattern(FILE *fp, int row, int col, int x_o, int y_o);
+unsigned char **pattern_106(FILE *fp, int row, int col, int x_o, int y_o);
+unsigned char **pattern_105(FILE *fp, int row, int col, int x_o, int y_o);
 void free_matrix(unsigned char **matrix, int row);
 
 #endif
